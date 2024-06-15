c5605b99beb2d215967da313bb1c2af6024101db
Pedro Sobrenome <pedroaugustolopesgomes@example.com>
2024-06-15 12:11:13 -0300

    feat: add route to get a task by id

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ app.get("/tasks", async (req, res) => {
   }
 });
 
+app.get("/tasks/:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+
+    const task = await TaskModel.findById(taskId);
+
+    if (!task) {
+      return res.status(404).send("Essa tarefa não existe ");
+    }
+
+    return res.status(200).send(task);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   try {
     const newTask = new TaskModel(req.body);
